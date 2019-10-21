960e94d966d86c7b7ef03234573c8143d1d71ab9
chrishasbeard <chrisholmes16@gmail.com>
2019-10-21 14:27:54 -0400

    translates the words but no sentence

--- Capstone PigLatin/Program.cs
+++ Capstone PigLatin/Program.cs
@@ -11,32 +11,33 @@ namespace Capstone_PigLatin
             Console.Write("Please enter a sentence to be translated: ");
             string word = Console.ReadLine();
             int index = 0;
+
+
             if (StartsWithVowel(word) == true)
-                {
+            {
                 Console.Write(word + "way");
-                }
-            else 
-                {
-                DoesNotStartWithVowel(word);
-                }
-            
-            for(int i =0; i< vowels.Length; i++)
-                {
-                if (word.StartsWith(vowels[i]))
-                    {
-                        Console.WriteLine(word + "way");
-                    }
-                }
-            index = word.IndexOfAny(vowels);
+            }
+            else if (StartsWithVowel(word) != true)
+            {
 
-            string firstPart = word.Substring(index);
-            string secondPart = word.Substring(0, index);
-            Console.WriteLine(firstPart + secondPart + "ay");
+                //for(int i =0; i< vowels.Length; i++)
+                //   {
+                //   if (word.StartsWith(vowels[i]))
+                //       {
+                //           Console.WriteLine(word + "way");
+                //       }
+                //   }
+                index = word.IndexOfAny(vowels);
+                string firstPart = word.Substring(index);
+                string secondPart = word.Substring(0, index);
+                Console.WriteLine(firstPart + secondPart + "ay");
+            }
         }
-        public static string DoesNotStartWithVowel(string word)
-        {
-
-        }
+        //public static string DoesNotStartWithVowel(string word)
+        //{
+        //   Console.Write(word + "ay");
+        //    return word;
+        //}
         public static bool StartsWithVowel(string word)
         {
             for (int i = 0; i < vowels.Length; i++)
---
+++ char work/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace char_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vowels = "aeiou";
+
+            Console.WriteLine("Give me a word!");
+            string word = Console.ReadLine();
+            if (vowels.Contains(word[0]))
+            {
+                for (int i = 0; )
+            }
+
+            foreach (char letter in word)
+            {
+
+            }
+        }
+    }
+}
